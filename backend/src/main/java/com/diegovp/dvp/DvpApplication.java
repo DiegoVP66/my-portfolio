b8b26a54851fd1dd520e7d79de9256640aa60307
DiegoVP66 <dvpdev33@gmail.com>
2022-06-29 19:20:18 -0300

    Backend created

---
+++ backend/src/main/java/com/diegovp/dvp/DvpApplication.java
@@ -0,0 +1,13 @@
+package com.diegovp.dvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diegovp/dvp/DvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegovp.dvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
