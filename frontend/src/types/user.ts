e629423fa25d29f404764940a1806f3d6d01b2a5
DiegoVP66 <dvpdev33@gmail.com>
2022-06-30 21:16:07 -0300

    Types && utils config

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,21 @@
+import { Role } from "./role";
+
+export type User = {
+  id: number;
+  email: string;
+};
+
+export type Users = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  roles: Role[];
+};
+
+export type UsersUpdate = {
+  id: number;
+  name: string;
+  email: string;
+  roles: Role[];
+};
--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,14 +1,65 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { getAuthData } from "./storage";
+import qs from "qs";
+type LoginData = {
+  username: string;
+  password: string;
+};
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? "diego";
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? "diego123";
+
 export const baseURL =
   process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
 
-  export const makeBackendRequest = (config: AxiosRequestConfig) => {
-    const headers = config.withCredentials
-      ? {
-          ...config.headers,
-          
-        }
-      : config.headers;
-  
-    return axios({ ...config, baseURL: baseURL, headers });
+export const makeBackendRequest = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: "Bearer " + getAuthData().access_token,
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: baseURL, headers });
+};
+
+export const requestBackendLogin = (loginData: LoginData) => {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Basic ${window.btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
   };
+
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
+
+  return axios({
+    method: "POST",
+    baseURL: baseURL,
+    url: "/oauth/token",
+    data,
+    headers,
+  });
+};
+
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      console.log("error");
+    }
+    return Promise.reject(error);
+  }
+);
